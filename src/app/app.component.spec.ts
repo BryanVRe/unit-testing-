c50a0be6f12f7de5d8194a04d30e8acb323f60a0
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2022-09-20 12:53:03 -0500

    add test fail #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent Test Suite', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'unit-testing'`, () => {
+  it(`should have as title 'unit-testing1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('unit-testing');
