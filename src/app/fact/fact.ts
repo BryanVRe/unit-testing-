507cbc98451fc0cda6a8c875d15b49257f5256fb
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2022-10-04 12:19:42 -0500

    update fact

--- src/app/fact/fact.ts
+++ src/app/fact/fact.ts
@@ -2,7 +2,7 @@ export function fact(n: number){
     if (n == 0) {
         return 1;
     }
-    if (n < 0 || n > 16) {
+    if (n < 0 || n >= 16) {
         return 0;
     }
     const result:number = n * fact(n-1);
